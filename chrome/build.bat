8d815de317da13d5921814d58ee4275f9514f1e8
Júlio C. Oliveira <talk@juliocesar.me>
2024-01-05 10:50:43 -0300

    Added copyright info comments in most files

--- chrome/build.bat
+++ chrome/build.bat
@@ -1,3 +1,19 @@
 @echo off
+
+goto comment
+
+YouTube Ad Auto-Skip is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+----------------------------------------------------------------------------------------------------------------------------
+Copyright (c) 2017 - 2024 Júlio C. Oliveira <http://www.juliocesar.me>
+
+This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+To view a copy of this license, visit <http://creativecommons.org/licenses/by-nc-sa/4.0/deed.en_US> .
+----------------------------------------------------------------------------------------------------------------------------
+
+$Id$
+Version 1.4.8 - 2023-12-08
+
+:comment
+
 web-ext build --ignore-files *.bat icon.fw.png --overwrite-dest --artifacts-dir "D:\Dropbox\Browser Extensions\GoogleChrome"
 pause
--- firefox/includes/youtube.js
+++ firefox/includes/youtube.js
@@ -1,3 +1,16 @@
+/*
+YouTube Ad Auto-Skip is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+----------------------------------------------------------------------------------------------------------------------------
+Copyright (c) 2017 - 2024 Júlio C. Oliveira <http://www.juliocesar.me>
+
+This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+To view a copy of this license, visit <http://creativecommons.org/licenses/by-nc-sa/4.0/deed.en_US> .
+----------------------------------------------------------------------------------------------------------------------------
+
+$Id$
+Version 1.4.8 - 2023-12-08
+*/
+
 // Define global variables
 var seconds = 0.10 * 1000;
 var closeId = 0;
